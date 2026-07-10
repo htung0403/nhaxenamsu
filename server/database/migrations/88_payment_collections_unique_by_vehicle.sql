f6576ba54f941ac13cefc8e619b9b93d2d2eed0c
htung0403 <htung0403@gmail.com>
2026-07-10 17:49:15 +0700

    fix deploy

--- server/database/migrations/88_payment_collections_unique_by_vehicle.sql
+++ server/database/migrations/88_payment_collections_unique_by_vehicle.sql
@@ -4,3 +4,5 @@ CREATE UNIQUE INDEX unique_active_collection
   ON public.payment_collections(delivery_order_id, vehicle_id)
   WHERE status IN ('submitted', 'confirmed', 'self_confirmed')
     AND delivery_order_id IS NOT NULL;
+
+    
